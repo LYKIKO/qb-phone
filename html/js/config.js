60267993368c915a0ab56afe59fd99abf9dd50cd
KIKO <lyheangzzff@gmail.com>
2025-10-15 11:37:00 +0700

    Add files via upload

---
+++ html/js/config.js
@@ -0,0 +1,15 @@
+Config = []
+
+Config.HeaderDisabledApps = [
+    "bank", 
+    "whatsapp", 
+    "garage",
+    "crypto",
+    "racing",
+    "houses",
+    "lawyers",
+    "services",
+    "trucker",
+]
+
+Config.DefaultCryptoPage = "general";
